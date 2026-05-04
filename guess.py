e1f1f30554d4dd2b43c618ce92dc118e4bb71145
SNEHA VISHWAKARMA <vishwakarmasneha7575@gmail.com>
2026-05-04 09:12:32 +0530

    Create guess.py
    NUMBER GUESSING GAME

---
+++ guess.py
@@ -0,0 +1,8 @@
+import random
+num = random.randint(1,10)
+guess == int (input("guess the number betwwen( 1 , 10): ")
+              
+ if guess == num:
+  print("you win")
+else:
+print("wrong!  the number was",num)
